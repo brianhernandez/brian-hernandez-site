4a5e5a39ea68f3bc6b18c7a709918c580775ab48
Brian Hernandez <brianhernandez@users.noreply.github.com>
2024-04-05 16:01:36 -0500

    Add Tailwind PrimeVue support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import path from 'path';
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['~/assets/styles/main.css'],
   modules: ['nuxt-primevue', '@nuxtjs/tailwindcss'],
+  primevue: {
+    options: {
+      unstyled: true,
+    },
+    importPT: { from: path.resolve(__dirname, './.primevue_presets/lara/') },
+  },
 });

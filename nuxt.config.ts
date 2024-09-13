327e1b4f46c0f3780b3fd17ac113f52fc132e8ec
Brian Hernandez <brianhernandez@users.noreply.github.com>
2024-09-13 11:33:51 -0500

    Add root redirect to Contra portfolio, Update caniuse-lite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import path from 'path';
+import path from "path";
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  extends: ['blog', 'core', 'site'],
+  extends: ["blog", "core", "site"],
   dir: {
-    assets: '~/core/assets',
-    layouts: '~/site/layouts',
-    pages: '~/site/pages',
-    plugins: '~/core/plugins',
+    assets: "~/core/assets",
+    layouts: "~/site/layouts",
+    pages: "~/site/pages",
+    plugins: "~/core/plugins",
   },
-  css: ['~/core/assets/styles/main.css'],
-  modules: ['nuxt-primevue', '@nuxtjs/tailwindcss'],
+  css: ["~/core/assets/styles/main.css"],
+  modules: ["nuxt-primevue", "@nuxtjs/tailwindcss"],
   primevue: {
     options: {
       unstyled: true,
     },
-    importPT: { from: path.resolve(__dirname, './.primevue_presets/lara/') },
+    importPT: { from: path.resolve(__dirname, "./.primevue_presets/lara/") },
   },
   routeRules: {
-    '/services-and-pricing': {
-      redirect: 'https://brianhernandez.framer.website',
+    "/": {
+      redirect: "https://brianhernandez.contra.com",
+    },
+    "/services-and-pricing": {
+      redirect: "https://brianhernandez.framer.website",
     },
   },
   runtimeConfig: {
     public: {
-      posthogPublicKey: 'phc_cSlajbmRxQt8Lzjq46wz7KJkHTqiGlGgcMg5jZ5bl9h',
-      posthogHost: 'https://us.i.posthog.com',
+      posthogPublicKey: "phc_cSlajbmRxQt8Lzjq46wz7KJkHTqiGlGgcMg5jZ5bl9h",
+      posthogHost: "https://us.i.posthog.com",
     },
   },
-  plugins: ['~/core/plugins/posthog.client.js'],
+  plugins: ["~/core/plugins/posthog.client.js"],
 });
+
+// TODO: add prettier-plugin-tailwindcss plugin
